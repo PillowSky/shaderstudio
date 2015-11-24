f17802098569679f3c52e78c8f7f66ed2b7d4a5b
Yuzo <hyzgog@gmail.com>
2015-11-24 16:16:47 +0800

    init version

---
+++ app.js
@@ -0,0 +1,15 @@
+require('coffee-script/register');
+
+var express = require('express');
+var config = require('./config/config');
+
+var app = express();
+require('./config/express')(app, config);
+
+if (require.main == module) {
+	app.listen(config.port, function () {
+		console.log('Express server listening on port ' + this.address().port);
+	});
+} else {
+	module.exports = app;
+}
