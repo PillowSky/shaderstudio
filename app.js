460e68972b6d84045dd3e78a37c89fef6a9bc75f
Yuzo <hyzgog@gmail.com>
2016-02-29 14:00:20 +0800

    submit mirror branch

--- app.js
+++ app.js
@@ -1,15 +1,40 @@
-require('coffee-script/register');
-
+var fs = require('fs');
+var http = require('http');
 var express = require('express');
-var config = require('./config/config');
+var httpProxy = require('http-proxy');
+
+var proxy = httpProxy.createProxyServer({});
+var shadertoyjson = JSON.parse(fs.readFileSync('shadertoy.json'));
 
 var app = express();
-require('./config/express')(app, config);
 
-if (require.main == module) {
-	app.listen(config.port, function () {
-		console.log('Express server listening on port ' + this.address().port);
+process.on('uncaughtException', function(error) {
+	console.error(error)
+});
+
+
+app.get('/', function(req, res){
+	res.sendfile('index.html');
+})
+
+app.get('/view/:filename', function(req, res){
+	res.header('Content-type', 'text/html');
+	res.end(fs.readFileSync('view/' + req.params.filename));
+})
+
+app.get('/css/shadertoy.css', function(req, res){
+	res.sendfile('shadertoy.css');
+})
+
+app.post('/shadertoy', function(req, res){
+	res.json(shadertoyjson);
+})
+
+app.use(function(req, res, next) {
+	proxy.web(req, res, {
+		target: 'http://www.shadertoy.com/'
 	});
-} else {
-	module.exports = app;
-}
+})
+
+app.listen(18080);
+
